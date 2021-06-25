8ea9c56bc02003c0b24915682fd462c0b52d9271
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-25 11:29:32 +0200

    Updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,5 +4,12 @@
  * My Script portfolio
  */
 document.addEventListener("DOMContentLoaded", () => {
-    //code
+    /* const CONF = new URL('https://cdn.jsdelivr.net/npm/particles.js@2.0.0/package.json');
+    console.log(CONF);
+    particlesJS.load('cover', 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/package.json', function() {
+        console.log('callback - particles.js config loaded');
+    }); */
+    particlesJS.load('cover', '../assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
