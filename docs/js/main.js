1993a81664f10617d583774aba305af7fb525b1b
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-25 15:07:17 +0200

    Updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,8 +7,8 @@
     const normalizeTransparency = value => value >= 1 ? 1 : value;
 
     const getParticlesNumber = value => {
-        if (window.innerWidth < 550 ) return 25;
-        else if (window.innerWidth < 750 ) return 45;
+        if (window.innerWidth < 550 ) return 35;
+        else if (window.innerWidth < 750 ) return 50;
         else if (window.innerWidth < 1050 ) return 65;
         else return 80;
     }
@@ -18,7 +18,7 @@
         const COVER  = document.getElementById("cover");
 
         PARTICLES_CONF.particles.number.value = getParticlesNumber(window.innerWidth);
-        if (window.innerWidth < 500) PARTICLES_CONF.particles.move.speed = 6;
+        if (window.innerWidth < 500) PARTICLES_CONF.particles.move.speed = 8;
     
         particlesJS('cover', PARTICLES_CONF);
     
