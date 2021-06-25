3dafd2a2ded412016ef4ad64bb333276b4e3b75e
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-25 12:37:34 +0200

    Updated

--- docs/js/lib/particles/particles_conf.js
+++ docs/js/lib/particles/particles_conf.js
@@ -1,7 +1,7 @@
 const PARTICLES_CONF = {
     "particles": {
       "number": {
-        "value": 60,
+        "value": 80,
         "density": {
           "enable": true,
           "value_area": 800
@@ -47,7 +47,7 @@ const PARTICLES_CONF = {
       },
       "line_linked": {
         "enable": true,
-        "distance": 300,
+        "distance": 200,
         "color": "#ffffff",
         "opacity": 0.4,
         "width": 2
--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,18 @@
  * 
  * My Script portfolio
  */
-document.addEventListener("DOMContentLoaded", () => {
-    particlesJS('cover', PARTICLES_CONF);
-});
+{
+    const normalizeTransparency = value => value >= 1 ? 1 : value;
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const HEADER = document.getElementsByTagName("header")[0];
+        const COVER  = document.getElementById("cover");
+    
+        particlesJS('cover', PARTICLES_CONF);
+    
+        document.addEventListener("scroll", () => {
+            //console.log(`rgb(40,40,40,${normalizeTransparency( (window.scrollY/COVER.clientHeight).toFixed(1) )})`);
+            HEADER.style.backgroundColor = `rgb(0,0,0,${normalizeTransparency( (window.scrollY/COVER.clientHeight).toFixed(1) )})`;
+        })
+    });
+}
