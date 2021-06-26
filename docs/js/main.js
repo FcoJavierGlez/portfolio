dc623c36b76df92ca2ad7f72f86bd7b2395ce0ba
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-26 23:19:28 +0200

    Updated

--- docs/js/main.js
+++ docs/js/main.js
@@ -41,6 +41,10 @@
               modifier: 1,
               slideShadows: true,
             },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
             pagination: {
               el: ".swiper-pagination",
             },
