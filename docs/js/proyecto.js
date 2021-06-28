490ccd0c2a25a281cefaeb4c2f6bb400d94c2d03
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-28 20:24:35 +0200

    Updated

---
+++ docs/js/proyecto.js
@@ -0,0 +1,14 @@
+/**
+ * @author Francisco Javier González Sabariego
+ * 
+ * My Script portfolio
+ */
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Jquey UI - Tooltip
+    $( function() {
+      $( document ).tooltip({
+        track: true
+      });
+    });
+});
