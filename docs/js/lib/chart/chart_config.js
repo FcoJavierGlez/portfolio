bce16fff0be9c33e016ab3efab3a0a477e51698e
FcoJavierGlez <FJGS_86@hotmail.com>
2021-06-29 12:15:21 +0200

    Added photo, description and charts

---
+++ docs/js/lib/chart/chart_config.js
@@ -0,0 +1,57 @@
+const CHART_CONFIG = {
+    config_js: {
+        type: 'doughnut',
+        data: data_js,
+        options: {}
+    },
+    config_ts: {
+        type: 'doughnut',
+        data: data_ts,
+        options: {}
+    },
+    config_php: {
+        type: 'doughnut',
+        data: data_php,
+        options: {}
+    },
+    config_java: {
+        type: 'doughnut',
+        data: data_java,
+        options: {}
+    },
+    config_python: {
+        type: 'doughnut',
+        data: data_python,
+        options: {}
+    },
+    config_react: {
+        type: 'doughnut',
+        data: data_react,
+        options: {}
+    },
+    config_angular: {
+        type: 'doughnut',
+        data: data_angular,
+        options: {}
+    },
+    config_express: {
+        type: 'doughnut',
+        data: data_express,
+        options: {}
+    },
+    config_socketio: {
+        type: 'doughnut',
+        data: data_socketio,
+        options: {}
+    },
+    config_mysql: {
+        type: 'doughnut',
+        data: data_mysql,
+        options: {}
+    },
+    config_mongodb: {
+        type: 'doughnut',
+        data: data_mongodb,
+        options: {}
+    },
+}
---
+++ docs/js/lib/chart/chart_data.js
@@ -0,0 +1,189 @@
+const data_js = {
+    labels: [
+        'JavaScript',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'JavaScript',
+        data: [80, 20],
+        backgroundColor: [
+            'rgb(235, 235, 20)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_ts = {
+    labels: [
+        'TypeScript',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'TypeScript',
+        data: [40, 60],
+        backgroundColor: [
+            'rgb(0, 100, 200)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_php = {
+    labels: [
+        'PHP',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'PHP',
+        data: [65, 25],
+        backgroundColor: [
+            'rgb(140, 160, 255)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_java = {
+    labels: [
+        'Java',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'Java',
+        data: [75, 25],
+        backgroundColor: [
+            'rgb(230, 170, 0)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_python = {
+    labels: [
+        'Python',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'Python',
+        data: [50, 50],
+        backgroundColor: [
+            'rgb(75, 200, 0)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+
+const data_react = {
+    labels: [
+        'React',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'React',
+        data: [30, 70],
+        backgroundColor: [
+            'rgb(100, 190, 255)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_angular = {
+    labels: [
+        'Angular',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'Angular',
+        data: [30, 70],
+        backgroundColor: [
+            'rgb(230, 25, 25)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_express = {
+    labels: [
+        'Express',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'Express',
+        data: [30, 70],
+        backgroundColor: [
+            'rgb(200, 200, 200)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_socketio = {
+    labels: [
+        'socket.io',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'socket.io',
+        data: [30, 70],
+        backgroundColor: [
+            'rgb(150, 150, 150)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+
+const data_mysql = {
+    labels: [
+        'MySQL',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'MySQL',
+        data: [70, 30],
+        backgroundColor: [
+            'darkturquoise',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
+const data_mongodb = {
+    labels: [
+        'MongoDB',
+        /* 'Unknown', */
+    ],
+    datasets: [{
+        label: 'MongoDB',
+        data: [35, 65],
+        backgroundColor: [
+            'rgb(0, 125, 0)',
+            'rgba(0,0,0,0)',
+        ],
+        hoverBorderColor: 'black',
+        hoverOffset: 0,
+        offSet: 0
+    }]
+};
